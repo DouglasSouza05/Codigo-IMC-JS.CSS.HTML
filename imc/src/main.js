6cc85bfadae8fc24d164ecf7949b2b1ab256aac1
DouglasSouza05 <douglasbsouza01@gmail.com>
2023-09-05 20:26:37 -0300

    Aplicando framework Svelte

---
+++ imc/src/main.js
@@ -0,0 +1,15 @@
+// import App from "./App.svelte";
+import Calculator from "./Calculator.svelte";
+
+// const app = new App({
+//   target: document.body,
+//   props: {
+//     name: "world",
+//   },
+// });
+
+const calculator = new Calculator({
+  target: document.body,
+});
+
+export default calculator;
